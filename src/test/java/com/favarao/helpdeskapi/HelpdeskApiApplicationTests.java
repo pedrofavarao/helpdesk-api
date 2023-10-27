10c32c59d0ba6e013219fca122af608385062906
pedrofavarao <pedrofavarao@gmail.com>
2023-10-27 11:35:40 -0300

    First commit

---
+++ src/test/java/com/favarao/helpdeskapi/HelpdeskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.favarao.helpdeskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
